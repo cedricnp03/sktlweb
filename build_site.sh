7e2e4ce7b6f75072e25cef7fa7f1936927d5de9d
MitsukiKanno <mitsukikanno0223@gmail.com>
2025-06-25 15:42:08 +0900

    add project description for indium

--- build_site.sh
+++ build_site.sh
@@ -16,10 +16,10 @@ read -p "Do you want to sync the site (y/n)? " sync_site
 if [ "$sync_site" == "y" ]; then
     rsync -avz -e ssh _site/ saktiwww@133.9.8.125:~/sakti
 fi
-
-read -p "Do you want to sync to GitHub (y/n)? " sync_github
-if [ "$sync_github" == "y" ]; then
-    git add .
-    git commit -m "Update site"
-    git push origin main
-fi
+#
+#read -p "Do you want to sync to GitHub (y/n)? " sync_github
+#if [ "$sync_github" == "y" ]; then
+#    git add .
+#    git commit -m "Update site"
+#    git push origin main
+#fi
